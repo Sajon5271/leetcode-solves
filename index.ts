44876d07c7b6fc68e2c8aa86cea90ce159e0ce7a
Sirajis Salekin Sajon <sajon5271@gmail.com>
2024-07-17 05:28:52 +0600

    Solved 347

--- index.ts
+++ index.ts
@@ -1,39 +1,14 @@
-function groupAnagrams(strs: string[]): string[][] {
-  const alphabetMap = getPrimeNumbersForLetters();
-
-  const resObj: Record<string, string[]> = {};
-
-  strs.forEach((str) => {
-    const mult = str
-      .split('')
-      .reduce((acc, curr) => acc * alphabetMap[curr], 1);
-    if (Array.isArray(resObj[mult])) {
-      resObj[mult].push(str);
-    } else {
-      resObj[mult] = [str];
-    }
+function topKFrequent(nums: number[], k: number): number[] {
+  const countObj: Record<string, number> = {};
+  nums.forEach((num) => {
+    if (countObj[num]) countObj[num]++;
+    else countObj[num] = 1;
   });
-  return Object.values(resObj);
+
+  return Object.entries(countObj)
+    .sort(([x, a], [y, b]) => b - a)
+    .slice(0, k)
+    .map(([val, _]) => parseInt(val));
 }
 
-function getPrimeNumbersForLetters() {
-  let primeNumber = 2;
-  const obj: Record<string, number> = {};
-  for (let i = 0; i < 26; i++) {
-    while (!isPrime(primeNumber)) {
-      primeNumber++;
-    }
-    obj[String.fromCharCode(i + 97)] = primeNumber;
-    primeNumber++;
-  }
-  return obj;
-}
-
-function isPrime(n: number) {
-  if (n < 2) return false;
-  if (n === 2) return true;
-  for (let i = 2; i < Math.sqrt(n) + 1; i++) {
-    if (n % i === 0) return false;
-  }
-  return true;
-}
+console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
